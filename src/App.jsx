8f772ff9bc08d269d09a7c5f4bce40f12d7bff78
Andrew Aposhian <aaposhian@mavenlink.com>
2023-05-12 12:33:37 -0600

    feat: layout character name and fields in header grid

--- src/App.jsx
+++ src/App.jsx
@@ -4,39 +4,45 @@ import './App.css'
 function App() {
   return (
       <>
-          <TextInput
-              label="Character Name"
-          />
           <Grid>
               <Grid.Col span={4}>
                   <TextInput
-                      label="Class & Level"
+                      label="Character Name"
                   />
               </Grid.Col>
-              <Grid.Col span={4}>
-                  <TextInput
-                      label="Background"
-                  />
-              </Grid.Col>
-              <Grid.Col span={4}>
-                  <TextInput
-                      label="Player Name"
-                  />
-              </Grid.Col>
-              <Grid.Col span={4}>
-                  <TextInput
-                      label="Race"
-                  />
-              </Grid.Col>
-              <Grid.Col span={4}>
-                  <TextInput
-                      label="Alignment"
-                  />
-              </Grid.Col>
-              <Grid.Col span={4}>
-                  <TextInput
-                      label="Experience Points"
-                  />
+              <Grid.Col span={8}>
+                  <Grid>
+                      <Grid.Col span={4}>
+                          <TextInput
+                              label="Class & Level"
+                          />
+                      </Grid.Col>
+                      <Grid.Col span={4}>
+                          <TextInput
+                              label="Background"
+                          />
+                      </Grid.Col>
+                      <Grid.Col span={4}>
+                          <TextInput
+                              label="Player Name"
+                          />
+                      </Grid.Col>
+                      <Grid.Col span={4}>
+                          <TextInput
+                              label="Race"
+                          />
+                      </Grid.Col>
+                      <Grid.Col span={4}>
+                          <TextInput
+                              label="Alignment"
+                          />
+                      </Grid.Col>
+                      <Grid.Col span={4}>
+                          <TextInput
+                              label="Experience Points"
+                          />
+                      </Grid.Col>
+                  </Grid>
               </Grid.Col>
           </Grid>
       </>
