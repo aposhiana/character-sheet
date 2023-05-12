b41c271fbcf0fb51fb9c1a51d7c828d7fd7039cd
Andrew Aposhian <aaposhian@mavenlink.com>
2023-05-12 12:24:37 -0600

    feat: add character header

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,29 @@ import './App.css'
 
 function App() {
   return (
-      <TextInput
-          label="Character Name"
-      />
+      <>
+          <TextInput
+              label="Character Name"
+          />
+          <TextInput
+              label="Class & Level"
+          />
+          <TextInput
+              label="Background"
+          />
+          <TextInput
+              label="Player Name"
+          />
+          <TextInput
+              label="Race"
+          />
+          <TextInput
+              label="Alignment"
+          />
+          <TextInput
+              label="Experience Points"
+          />
+      </>
   )
 }
 
