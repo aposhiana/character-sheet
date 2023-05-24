751e0cee99294c5c5fab393db2abb6fd13096679
Andrew Aposhian <aaposhian@mavenlink.com>
2023-05-24 13:51:09 -0600

    feat: layout abilities in column to the left

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import './CharacterHeader.jsx'
 import './App.css'
 import CharacterHeader from "./CharacterHeader";
 import Abilities from "./Abilities.jsx";
+import { Grid } from '@mantine/core';
 
 function App() {
   return (
       <>
         <CharacterHeader />
-        <Abilities />
+          <Grid>
+              <Grid.Col span={2}>
+                  <Abilities />
+              </Grid.Col>
+          </Grid>
       </>
   )
 }
