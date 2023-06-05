0297f069d988085c09bc828ac39b88541e6200fb
Andrew Aposhian <aaposhian@mavenlink.com>
2023-06-05 09:18:11 -0600

    feat: add saving throws checkboxes

--- src/SavingThrows.jsx
+++ src/SavingThrows.jsx
@@ -1,14 +1,22 @@
-import {Text} from "@mantine/core";
+import {Checkbox, Text, Box} from "@mantine/core";
 
 export default function SavingThrows() {
     return (
-        <Text
-            sx={{ fontFamily: 'Greycliff CF, sans-serif' }}
-            ta="center"
-            fz="l"
-            fw={700}
-        >
-            Saving Throws
-        </Text>
+        <Box m="lg">
+            <Checkbox label={'Strength'} />
+            <Checkbox label={'Dexterity '} />
+            <Checkbox label={'Constitution'} />
+            <Checkbox label={'Intelligence'} />
+            <Checkbox label={'Wisdom'} />
+            <Checkbox label={'Charisma'} />
+            <Text
+                sx={{ fontFamily: 'Greycliff CF, sans-serif' }}
+                ta="center"
+                fz="lg"
+                fw={700}
+            >
+                Saving Throws
+            </Text>
+        </Box>
     );
 }
